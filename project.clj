cff3237090351f88dd2e6facf9c571d86b579072
Luiz Guilherme D'Abruzzo Pereira <luiz290788@gmail.com>
2015-09-29 10:19:08 -0300

    first try to make everything async

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [http-kit "2.1.18"]
-                 [enlive "1.1.5"]])
+                 [enlive "1.1.5"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]])
--- src/wiki_game/core.clj
+++ src/wiki_game/core.clj
@@ -1,6 +1,7 @@
 (ns wiki-game.core
-  (:require [org.httpkit.client :as http])
-  (:use net.cgrand.enlive-html))
+  (:require [org.httpkit.client :as http]
+            [clojure.core.async :as a])
+  (:use [net.cgrand.enlive-html]))
 
 (defn parse-html
   [html]
@@ -9,10 +10,10 @@
 (defn normalize-url
   [url]
   (let [hash-index (.indexOf url "#")]
-    (if (>= hash-index 0)
-      (.substring url 0 hash-index)
-      url
-      )
+    (.toLowerCase (if (>= hash-index 0)
+                    (.substring url 0 hash-index)
+                    url
+                    ))
     )
   )
 
@@ -25,6 +26,17 @@
   [url]
   (:body @(http/get (str "http://thewikigame.com" url) {:headers {"Cookie" "sessionid=jkpxxzzvbovx9xgcma2il0q1yueswc8n; "}})))
 
+(defn get-body-async
+  [url]
+  (let [body-chan (a/chan)]
+    (a/go
+      (let [body (get-body url)]
+        (if (not (nil? body))
+          (a/>! body-chan body))
+        (a/close! body-chan)))
+    body-chan)
+  )
+
 (defn create-vertex
   [origin dest]
   {dest origin})
@@ -35,29 +47,49 @@
     (get-body)
     (parse-html)
     (extract-links)
-    (set))
+    (set)
+    (map #(conj [url] %)))
   )
+
+(defn links-async
+  [url]
+  (let [output (a/chan)]
+    (a/pipeline-blocking 1 output
+                (comp (map parse-html)
+                      (map extract-links)
+                      (mapcat distinct)
+                      (map normalize-url)
+                      (filter #(< (.indexOf % ":") 0))
+                      (map #(conj [url] %)))
+                (get-body-async url))
+    output))
 
 (defn build-graph
   [start end]
-  (loop [links-graph {}
-         to-do [start]]
-    (if (or (contains? links-graph end) (= 0 (count to-do)))
-      links-graph
-      (let [current-path (first to-do)
-            page-links (filter #(not (or (contains? links-graph %) (contains? to-do %))) (links current-path))]
-        (recur (reduce #(assoc %1 %2 current-path) links-graph page-links)
-               (into [] (concat (next to-do) page-links)))
-        )
-      )
-    )
-  )
+  (let 
+    [to-do (a/chan (a/buffer 2048))
+     normalized-end (normalize-url end)]
+    (a/>!! to-do [nil (normalize-url start)])
+    (a/<!! (a/go-loop 
+             [[from current-path] (a/<! to-do)
+              links-graph {}]
+             (if (nil? to-do)
+               nil
+               (if (contains? links-graph normalized-end)
+                 links-graph
+                 (if (contains? links-graph current-path)
+                   (recur (a/<! to-do) links-graph)
+                   (let [links-chan (links-async current-path)]
+                     (a/pipe links-chan to-do false)
+                     (if (not (nil? from))
+                       (recur (a/<! to-do) (assoc links-graph current-path from))
+                       (recur (a/<! to-do) links-graph))))))))))
 
 
 (defn wikipedia-min-path
   [start end]
-  (loop [current-step end
-         path-head start
+  (loop [current-step (normalize-url end)
+         path-head (normalize-url start)
          path []
          graph (build-graph start end)]
     (if (= current-step path-head)
